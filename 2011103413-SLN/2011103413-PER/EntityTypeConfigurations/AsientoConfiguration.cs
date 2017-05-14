c2478249641cec98b0a5720075f9b8a7c61052e0
Aaron carrasco <aaroncarrasco2017@gmail.com>
2017-05-13 19:21:07 -0500

    LC2

---
+++ 2011103413-SLN/2011103413-PER/EntityTypeConfigurations/AsientoConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using _2013211290_ENT;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2013211290_PER.EntityTypeConfigurations
+{
+    public class AsientoConfiguration : EntityTypeConfiguration<Asiento>
+    {
+
+        public AsientoConfiguration()
+        {
+            Property(v => v.AsientoId)
+                .IsRequired();
+
+
+            HasRequired(v => v.Cinturon)
+                .WithRequiredPrincipal(c => c.Asiento)
+                ;
+
+        }
+
+    }
+
+    
+}
